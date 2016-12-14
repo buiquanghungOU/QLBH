35415b24008c2afd5912a89947242e60a421f04c
PhuongQuynh <p.quynh27@gmail.com>
2016-12-14 09:56:54 +0700

    UpdateV2

--- CuaHangDT/Program.cs
+++ CuaHangDT/Program.cs
@@ -16,7 +16,7 @@ namespace CuaHangDT
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmQuanLy());
+            Application.Run(new frmLogin());
         }
     }
 }
