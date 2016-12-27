1d0578f8a63d3fc2f8441d50d42a7b8615dd9f52
PhuongQuynh <p.quynh27@gmail.com>
2016-12-27 12:11:00 +0700

    Code FrmTim_ThanhToan

--- CuaHangDT/FrmThanhToan.Designer.cs
+++ CuaHangDT/FrmThanhToan.Designer.cs
@@ -180,6 +180,7 @@
             this.button2.TabIndex = 3;
             this.button2.Text = "TÌM KIẾM";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // txtDonGia
             // 
