90990b37279d4480cb20d6c81499e23545307951
qh <lephuong0796@gmail.com>
2016-12-27 22:23:08 +0700

    Report

--- CuaHangDT/FrmThongKeDT.Designer.cs
+++ CuaHangDT/FrmThongKeDT.Designer.cs
@@ -28,93 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.button2 = new System.Windows.Forms.Button();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.label2 = new System.Windows.Forms.Label();
+            this.components = new System.ComponentModel.Container();
+            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource1 = new Microsoft.Reporting.WinForms.ReportDataSource();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.button1 = new System.Windows.Forms.Button();
-            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
             this.label1 = new System.Windows.Forms.Label();
             this.lbMain = new System.Windows.Forms.Label();
-            this.reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
-            this.groupBox2.SuspendLayout();
+            this.rpviewTKDT = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.cbNgayLap = new System.Windows.Forms.ComboBox();
+            this.DataSetTKDT = new CuaHangDT.DataSetTKDT();
+            this.DataTableTKDTBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.DataTableTKDTTableAdapter = new CuaHangDT.DataSetTKDTTableAdapters.DataTableTKDTTableAdapter();
             this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.DataSetTKDT)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DataTableTKDTBindingSource)).BeginInit();
             this.SuspendLayout();
-            // 
-            // groupBox2
-            // 
-            this.groupBox2.Controls.Add(this.button2);
-            this.groupBox2.Controls.Add(this.comboBox1);
-            this.groupBox2.Controls.Add(this.label2);
-            this.groupBox2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.groupBox2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
-            this.groupBox2.Location = new System.Drawing.Point(15, 129);
-            this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(386, 84);
-            this.groupBox2.TabIndex = 10;
-            this.groupBox2.TabStop = false;
-            this.groupBox2.Text = "Theo Hãng Sản Xuất";
-            // 
-            // button2
-            // 
-            this.button2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(257, 32);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(83, 32);
-            this.button2.TabIndex = 5;
-            this.button2.Text = "Tìm";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
-            // comboBox1
-            // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(108, 32);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(111, 27);
-            this.comboBox1.TabIndex = 3;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(15, 34);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(75, 19);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "Tên HSX:";
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.cbNgayLap);
             this.groupBox1.Controls.Add(this.button1);
-            this.groupBox1.Controls.Add(this.dateTimePicker1);
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.groupBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
             this.groupBox1.Location = new System.Drawing.Point(13, 45);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(388, 59);
+            this.groupBox1.Size = new System.Drawing.Size(477, 68);
             this.groupBox1.TabIndex = 9;
             this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Theo Ngày";
+            this.groupBox1.Text = "Thống Kê Theo";
             // 
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(259, 19);
+            this.button1.Location = new System.Drawing.Point(343, 18);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(83, 32);
             this.button1.TabIndex = 5;
-            this.button1.Text = "Tìm";
+            this.button1.Text = "Thống Kê";
             this.button1.UseVisualStyleBackColor = true;
-            // 
-            // dateTimePicker1
-            // 
-            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
-            this.dateTimePicker1.Location = new System.Drawing.Point(110, 25);
-            this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(111, 26);
-            this.dateTimePicker1.TabIndex = 4;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // label1
             // 
@@ -122,9 +75,9 @@
             this.label1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(17, 25);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(50, 19);
+            this.label1.Size = new System.Drawing.Size(108, 19);
             this.label1.TabIndex = 2;
-            this.label1.Text = "Ngày:";
+            this.label1.Text = "Ngày Lập HD:";
             // 
             // lbMain
             // 
@@ -133,53 +86,75 @@
             this.lbMain.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
             this.lbMain.Location = new System.Drawing.Point(0, 0);
             this.lbMain.Name = "lbMain";
-            this.lbMain.Size = new System.Drawing.Size(716, 42);
+            this.lbMain.Size = new System.Drawing.Size(898, 128);
             this.lbMain.TabIndex = 8;
             this.lbMain.Text = "THỐNG KÊ DOANH THU";
             this.lbMain.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
-            // reportViewer1
+            // rpviewTKDT
             // 
-            this.reportViewer1.Location = new System.Drawing.Point(15, 235);
-            this.reportViewer1.Name = "reportViewer1";
-            this.reportViewer1.Size = new System.Drawing.Size(663, 200);
-            this.reportViewer1.TabIndex = 11;
+            this.rpviewTKDT.Dock = System.Windows.Forms.DockStyle.Fill;
+            reportDataSource1.Name = "DataSet1";
+            reportDataSource1.Value = this.DataTableTKDTBindingSource;
+            this.rpviewTKDT.LocalReport.DataSources.Add(reportDataSource1);
+            this.rpviewTKDT.LocalReport.ReportEmbeddedResource = "CuaHangDT.Report1.rdlc";
+            this.rpviewTKDT.Location = new System.Drawing.Point(0, 128);
+            this.rpviewTKDT.Name = "rpviewTKDT";
+            this.rpviewTKDT.Size = new System.Drawing.Size(898, 348);
+            this.rpviewTKDT.TabIndex = 11;
+            // 
+            // cbNgayLap
+            // 
+            this.cbNgayLap.FormattingEnabled = true;
+            this.cbNgayLap.Location = new System.Drawing.Point(165, 22);
+            this.cbNgayLap.Name = "cbNgayLap";
+            this.cbNgayLap.Size = new System.Drawing.Size(127, 27);
+            this.cbNgayLap.TabIndex = 6;
+            // 
+            // DataSetTKDT
+            // 
+            this.DataSetTKDT.DataSetName = "DataSetTKDT";
+            this.DataSetTKDT.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // DataTableTKDTBindingSource
+            // 
+            this.DataTableTKDTBindingSource.DataMember = "DataTableTKDT";
+            this.DataTableTKDTBindingSource.DataSource = this.DataSetTKDT;
+            // 
+            // DataTableTKDTTableAdapter
+            // 
+            this.DataTableTKDTTableAdapter.ClearBeforeFill = true;
             // 
             // FrmThongKeDT
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(716, 470);
-            this.Controls.Add(this.reportViewer1);
-            this.Controls.Add(this.groupBox2);
+            this.ClientSize = new System.Drawing.Size(898, 476);
+            this.Controls.Add(this.rpviewTKDT);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.lbMain);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.MaximizeBox = false;
-            this.MinimizeBox = false;
             this.Name = "FrmThongKeDT";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "FrmThongKeDT";
+            this.Text = "Thống Kê Doanh Thu";
             this.Load += new System.EventHandler(this.FrmThongKeDT_Load);
-            this.groupBox2.ResumeLayout(false);
-            this.groupBox2.PerformLayout();
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.DataSetTKDT)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DataTableTKDTBindingSource)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.GroupBox groupBox2;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.DateTimePicker dateTimePicker1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label lbMain;
-        private Microsoft.Reporting.WinForms.ReportViewer reportViewer1;
+        private Microsoft.Reporting.WinForms.ReportViewer rpviewTKDT;
+        private System.Windows.Forms.ComboBox cbNgayLap;
+        private System.Windows.Forms.BindingSource DataTableTKDTBindingSource;
+        private DataSetTKDT DataSetTKDT;
+        private DataSetTKDTTableAdapters.DataTableTKDTTableAdapter DataTableTKDTTableAdapter;
     }
 }
--- CuaHangDT/FrmThongKeDT.cs
+++ CuaHangDT/FrmThongKeDT.cs
@@ -7,11 +7,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.Reporting.WinForms;
+using System.Data.SqlClient;
+using System.Configuration;
 
 namespace CuaHangDT
 {
     public partial class FrmThongKeDT : Form
     {
+        string cnStr = "";
+        SqlConnection cn;
+        SqlDataAdapter da;
+
         public FrmThongKeDT()
         {
             InitializeComponent();
@@ -19,8 +26,26 @@ namespace CuaHangDT
 
         private void FrmThongKeDT_Load(object sender, EventArgs e)
         {
+            cnStr = ConfigurationManager.ConnectionStrings["cnStr"].ConnectionString;
+            cn = new SqlConnection(cnStr);
+            LoadComboBoxNgayLapHD();
+           
+        }
+        private void LoadComboBoxNgayLapHD()
+         {
+             da = new SqlDataAdapter("SELECT * FROM HOADON", cn);
+             DataTable table = new DataTable();
+             da.Fill(table);
+             da.Dispose();
+             cbNgayLap.DataSource = table;//gán dữ liệu nguồn
 
-            this.reportViewer1.RefreshReport();
+             cbNgayLap.ValueMember = "NgayLapHD";
+         }
+        private void button1_Click(object sender, EventArgs e)
+        {
+             this.DataTableTKDTTableAdapter.Fill(this.DataSetTKDT.DataTableTKDT,cbNgayLap.SelectedValue.ToString());
+
+            this.rpviewTKDT.RefreshReport();
         }
     }
 }
